57ff17445034f72c029ead1c92200f44c9b87147
oliviawyy <olivia.c@aluno.senai.br>
2026-03-06 14:29:47 -0300

    estudando

--- classes-objetos/03objetos.js
+++ classes-objetos/03objetos.js
@@ -26,7 +26,30 @@ let quadrado2 = {
 let quadrado3 = [{}];
 // array, e na posicao 0 do array eu tenho o objeto
 
+// 01 - criar um objeto senai
+const senai = {
+    codigo: 603,
+    cidade: 'Araraquara'
+};
+
+console.log('--------------');
+
+senai.telefone = 1633035252;
+senai.rua = 'Hugo Negrini';
+delete senai.rua;
+
+senai.endereco = {rua:'Hugo Negrini', numero: 60, bairro: 'Quitandinha'};
+senai.alunos = ['SESI', 'EEBA', 'comunidade'];
+
+senai.endereco.numero = 62;
+senai.alunos[2] = "tag"
+console.log(senai);
+
+
+
 // 02 - criar funcoes (metodos area e perimetro)
 // 03 - entender o contexto this
 // 04 - escrever de forma abreviada
 // 05 - acessar os dados do objeto
+
+console.log(quadrado2.perimetro(4));
---
+++ classes-objetos/04objeto-metodos.js
@@ -0,0 +1,13 @@
+// metodo é uma funcao denteo de um objeyo
+const senai = {
+    codigo: 603,
+    cidade: 'Araraquara',
+
+    descreverEscola: () => {
+        // a palavra reserva this se refere ao proprio objeto, ou seja, senai
+        // é usado para acessar propriedades e metodos do objeto corrente.
+        console.log(`O código ${senai.codigo} pertence a escola do senai de ${senai.cidade}`)
+    }
+};
+
+console.log(senai.descreverEscola());
